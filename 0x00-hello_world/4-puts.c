0a302b87f8765265a80569192ed35a6fc1074726
ChelseaBun <chelseameyong@gmail.com>
2023-06-17 13:43:03 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *  main - A program that prints a line with functions
- * 
+ * main - A program that prints a line with puts functions
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle
-			");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
