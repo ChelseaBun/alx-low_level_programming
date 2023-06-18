2da5c84a1efe00d6a9edcebf732fa56518765e29
ChelseaBun <chelseameyong@gmail.com>
2023-06-18 11:51:43 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Entry point 
+ * main - A c program that prints a sentence & a new line to the standard error
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2,
-			"and that piece of art is useful" - Dora Korpar, 2015-10-19\n" , 59);
-	return (10;
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
